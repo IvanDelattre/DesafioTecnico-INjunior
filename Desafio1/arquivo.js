ebdb405df421f5a7e484339d5f67271014b91d3f
ivanc <ivandelattre7@gmail.com>
2024-12-24 10:21:39 -0300

    feat(Desafio1): add codigo desafio1

--- Desafio1/arquivo.js
+++ Desafio1/arquivo.js
@@ -1,4 +1,30 @@
+let a = Number( prompt("Dgite o 'a' da equação quadrática na forma : a(x^2) + bx + c = 0") );
+let b = Number ( prompt("Dgite o 'b' da equação quadrática na forma : a(x^2) + bx + c = 0") ) ;
+let c = Number( prompt("Dgite o 'c' da equação quadrática na forma : a(x^2) + bx + c = 0") ) ;
+
+roots(a , b , c) ;
+
+function roots( a , b , c){
+    let value = discriminant(a , b , c);
+
+    if( value < 0) {
+        
+        console.log("Não há raízes reais.");
+        return  ;
+    }
+    if( value == 0 ){
+        console.log ( `Única Raiz real: ${(-b)/ (2 * a)}`) ;
+    }
+
+    if( value > 0 ){
+        value = Math.sqrt(value);
+        console.log(`Primeira raiz real ${ (-b + value)/ (2 *a)  }.\nSegunda raiz real ${  (-b - value)/ (2 *a)  }`  )
+    }
+
+}
 
 
-
+function discriminant( a , b , c){
+    return (b**2) - (4 * a * c) ;
+}
 
