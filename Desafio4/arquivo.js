d674d446f0520433161b71995c2c474fa3111621
ivanc <ivandelattre7@gmail.com>
2024-12-24 12:28:20 -0300

    feat(Desafio4): resolucao desafio4

---
+++ Desafio4/arquivo.js
@@ -0,0 +1,17 @@
+
+let money = Number ( prompt("Digite valor da Carteira: ") )  ;
+
+
+convertion( money ) ; 
+
+
+function convertion(money ){
+
+    console.log(`Valor em dólar ${money/5.7} \nValor em euro ${money/6.1} `);
+
+}
+
+
+
+
+
