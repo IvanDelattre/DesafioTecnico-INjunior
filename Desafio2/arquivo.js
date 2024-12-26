7e4231551e77b10164a1de8e369aa16183d0403f
ivanc <ivandelattre7@gmail.com>
2024-12-26 07:32:19 -0300

    feat(desafio2): resolucao desafio2

--- Desafio2/arquivo.js
+++ Desafio2/arquivo.js
@@ -1,7 +1,4 @@
-
-//fazer questão 2 .
-
-function Fatorial(a){
+function fatorial(a){
     
     if( a < 0 || a - parseInt(a) > 0) throw new Error("Números válidos devem ser Inteiros e Positivos");
 
@@ -12,5 +9,27 @@ function Fatorial(a){
     return result ;
 }
 
+let continueLoop = true;
+while( continueLoop){
+    let option = prompt("Deseja calcular o fatorial de algum número: [s/n] ou [S/N]") ; 
+    
+    option = option.toLowerCase() ; 
 
 
+    switch(option){
+        case "s" :
+            let number =Number (  prompt("Digite número que se deseja calcular o fatorial: ")  ) ;
+            try{
+                console.log(`Fatorial de ${number} é ${ fatorial(number)} `);     
+            }catch(e){
+                console.log(e.message) ; 
+            }
+            break;
+        case "n":
+            continueLoop = false; 
+            break;
+        default:
+            console.log("Opção inválida");      
+    }
+
+}
